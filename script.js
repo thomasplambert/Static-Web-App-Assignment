a46f62bd3354379c923d01e3586a18f5c4f050fa
Thomas Lambert <tplambert@uncg.edu>
2026-02-03 20:43:01 -0500

    added all necessary pages and WIP layout for home.

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // Listen for scroll events
-window.addEventListener('scroll', function() {
-  
+function updateHooty(){
   // Calculate how far down the page we've scrolled (0 to 1)
   let scrollPercent = window.scrollY / (document.body.scrollHeight - window.innerHeight);
   
@@ -13,5 +12,8 @@ window.addEventListener('scroll', function() {
   
   // Stretch Hooty's body
   document.querySelector('.hooty-body').style.height = (headPosition+33) + 'px';
-});
+}
+
+window.addEventListener('scroll', updateHooty);
+
 updateHooty();
